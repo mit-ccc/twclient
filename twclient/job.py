410b25345fea61f1dbdeb8c0907e865bf68fae6b
William Brannon <will.brannon@gmail.com>
2020-09-17 02:03:41 -0400

    refactor FollowGraphJob

--- twclient/job.py
+++ twclient/job.py
@@ -163,10 +163,15 @@ class ApiJob(Job):
 
         self.load_batch_size = load_batch_size
 
+    def resolve_users(self, mode='raise'):
+        for target in self.targets:
+            target.resolve(context=self, mode=mode)
+
+        return [u for u in it.chain(*[t.users for t in self.targets])]
+
 class UserInfoJob(ApiJob):
     def run(self):
-        for target in self.targets:
-            target.resolve(context=self, mode='rehydrate')
+        self.resolve_users(mode='rehydrate')
 
         self.session.commit()
 
@@ -183,10 +188,7 @@ class TweetsJob(ApiJob):
         self.old_tweets = old_tweets
 
     def run(self):
-        for target in self.targets:
-            target.resolve(context=self, mode='raise')
-
-        users = [u for u in it.chain(*[t.users for t in self.targets])]
+        users = self.resolve_users(mode='raise')
 
         n_items = 0
         for i, user in enumerate(users):
@@ -224,6 +226,13 @@ class TweetsJob(ApiJob):
             self.session.commit()
 
 class FollowGraphJob(ApiJob):
+    def __init__(self, **kwargs):
+        robust = kwargs.pop('robust', True)
+
+        super(FollowGraphJob, self).__init__(**kwargs)
+
+        self.robust = robust
+
     @property
     @abstractmethod
     def direction(self):
@@ -235,140 +244,177 @@ class FollowGraphJob(ApiJob):
         raise NotImplementedError()
 
     @property
+    def api_method_name(self):
+        return self.direction + '_ids'
+
+    @property
     def target_user_column(self):
         cols = {'source_user_id', 'target_user_id'}
         return list(cols - {self.api_data_column})[0]
 
     @property
-    def api_method_name(self):
-        return self.direction + '_ids'
-
-    @property
     def api_method(self):
         return getattr(self.api, self.api_method_name)
 
-    def insert_one_by_one(self, model, rows):
+    def load_edges_for(self, user):
+        ids = self.api_method(user_id=user.user_id)
+        ids = ut.grouper(ids, self.load_batch_size)
+
+        # NOTE tl;dr the commit semantics here are complicated and depend on the
+        # database, but the details shouldn't matter. Depending on the DB,
+        # clearing the stg table may or may not make a commit; depending on the
+        # setting of self.robust, self.insert_stg_batch may or may not do so as
+        # well. BUT both of these affect only data in the stg table; if an error
+        # leaves it in an inconsistent state, we don't care. Whether there are 0
+        # or more than 0 commits up to the end of the for loop below, there
+        # aren't any during the call to process_stg_data_for, which is the only
+        # part of this that modifies main data tables. So that call happens
+        # atomically, which is what we care about.
+
+        self.clear_stg_table()
+
+        n_items = 0
+        for j, batch in enumerate(ids):
+            msg = 'Running {0} batch {1}, within-user cumulative edges {2}'
+            msg = msg.format(type(self), j + 1, n_items)
+            logger.debug(msg)
+
+            n_items += self.insert_stg_batch(user, batch)
+
+        self.process_stg_data_for(user)
+
+        return n_items
+
+    def clear_stg_table(self):
+        # this is much, much faster than .delete() / DELETE FROM <tbl>, but not
+        # transactional on many DBs
+        md.StgFollow.__table__.drop(self.session.get_bind())
+        md.StgFollow.__table__.create(self.session.get_bind())
+
+    def insert_stg_batch(self, user, api_user_ids):
+        rows = [
+            {self.api_data_column: t, self.target_user_column: user.user_id}
+            for t in api_user_ids
+        ]
+
+        try:
+            self.session.bulk_insert_mappings(md.StgFollow, rows)
+        except sa.exc.IntegrityError:
+            if self.robust:
+                n_items = self.insert_stg_batch_robust(md.StgFollow, rows)
+            else:
+                raise
+        else:
+            n_items = len(batch)
+
+        # NOTE committing slows things down, but without it, we'd lose every
+        # row loaded before one of these duplicate key problems
+        if robust:
+            self.session.commit()
+
+        return n_items
+
+    def insert_stg_batch_robust(self, rows):
         nrows = 0
 
+        # NOTE Twitter sometimes returns the same ID more than once.
+        # This happens rarely (probably b/c of eventual consistency), so we
+        # don't need to worry too hard about performance in handling it.
+        # Thus: use bulk inserts, but catch the duplicate key error and,
+        # when handling it, re-attempt inserts of the same rows one by one,
+        # discarding any that raise the duplicate key error.
         for row in rows:
             try:
-                model.__table__.insert().values(**row)
+                md.StgFollow.__table__.insert().values(**row)
                 nrows += 1
             except sa.exc.IntegrityError:
-                msg = 'Ignoring duplicate follow-graph edge {0} from Twitter'
+                msg = 'Ignoring integrity error (likely duplicate) on edge {0}'
                 logger.debug(msg.format(row))
 
         return nrows
 
-    # FIXME refactor and think through commit semantics
-    def run(self):
-        for target in self.targets:
-            # NOTE that here (unlike in TweetsJob), you can get gnarly primary
-            # key integrity errors on the user table if this isn't mode='raise':
-            # merging in an md.User object for a given user and then running an
-            # insert against the user table may try to insert the same row again
-            # at commit if one of the User objects is for a row already loaded
-            # by the insert. (That is, if fetching users A and B, user B hadn't
-            # already been loaded and were hydrated here, and A follows B.) In
-            # this case, if this were not to be mode='raise' in the future, the
-            # easy thing to do is call self.session.flush() afterward.
-            target.resolve(context=self, mode='raise')
+    def process_stg_data_for(self, user):
+        ##
+        ## 1. Load any new users to user table
+        ##
 
-        users = [u for u in it.chain(*[t.users for t in self.targets])]
+        flt = self.session.query(md.User).filter(
+            md.User.user_id == getattr(md.StgFollow, self.api_data_column)
+        ).correlate(md.StgFollow)
+
+        # We don't need to worry about inserting the same user_id value
+        # that's already in the user.user_id object (and causing a primary
+        # key integrity error on the user table) because that user_id is
+        # already in the self.target_user_column column; it would only also
+        # appear in the self.api_data_column column if you could follow
+        # yourself on Twitter, which you can't.
+        ins = md.User.__table__.insert().from_select(
+            ['user_id'],
+            self.session.query(
+                getattr(md.StgFollow, self.api_data_column)
+            ).filter(~flt.exists())
+        )
+
+        self.session.execute(ins)
+
+        ##
+        ## 2. Load new edges to follow table with valid_end_dt of null
+        ##
+
+        flt = self.session.query(md.Follow).filter(sa.and_(
+            md.Follow.valid_end_dt == None,
+            md.Follow.source_user_id == md.StgFollow.source_user_id,
+            md.Follow.target_user_id == md.StgFollow.target_user_id
+        )).correlate(md.StgFollow)
+
+        ins = md.Follow.__table__.insert().from_select(
+            ['source_user_id', 'target_user_id'],
+            self.session.query(
+                md.StgFollow.source_user_id,
+                md.StgFollow.target_user_id
+            ).filter(~flt.exists())
+        )
+
+        self.session.execute(ins)
+
+        ##
+        ## 3. Mark edges no longer present as expired (valid_end_dt := now())
+        ##
+
+        flt = self.session.query(md.StgFollow).filter(sa.and_(
+            md.StgFollow.source_user_id == md.Follow.source_user_id,
+            md.StgFollow.target_user_id == md.Follow.target_user_id
+        )).correlate(md.Follow)
+
+        upd = md.Follow.__table__.update().where(sa.and_(
+            md.Follow.valid_end_dt == None,
+            getattr(md.Follow, self.target_user_column) == user.user_id,
+
+            ~flt.exists()
+        )).values(valid_end_dt=sa.func.now())
+
+        self.session.execute(upd)
+
+    # NOTE that here (unlike in TweetsJob), you can get gnarly primary
+    # key integrity errors on the user table if resolve_users has mode
+    # != 'raise': merging in an md.User object for a given user and then
+    # running an insert against the user table may try to insert the same
+    # row again at commit if one of the User objects is for a row already
+    # loaded by the insert. (That is, if fetching users A and B, user B
+    # hadn't already been loaded and were hydrated here, and A follows B.)
+    # In this case, if this were not to be mode='raise' in the future, the
+    # easy thing to do is call self.session.flush() afterward.
+    def run(self):
+        users = self.resolve_users(mode='raise')
 
         n_items = 0
         for i, user in enumerate(users):
-            msg = 'Processing user_id {0} ({1} / {2}), cumulative edges {3}'
-            logger.info(msg.format(user.user_id, i + 1, len(users), n_items))
+            msg = 'Processing user_id {0} ({1} / {2}), ' \
+                  'across-user cumulative edges {3}'
+            msg = msg.format(user.user_id, i + 1, len(users), n_items)
+            logger.info(msg)
 
-            ids = self.api_method(user_id=user.user_id)
-            ids = ut.grouper(ids, self.load_batch_size)
-
-            # clear the stg table - this is much faster than '.delete()'
-            # but has the downside of not being transactional on many DBs
-            md.StgFollow.__table__.drop(self.session.get_bind())
-            md.StgFollow.__table__.create(self.session.get_bind())
-
-            # NOTE Twitter sometimes returns the same ID more than once.
-            # This happens rarely (probably b/c of eventual consistency), so we
-            # don't need to worry too hard about performance in handling it.
-            # Thus: use bulk inserts, but catch the duplicate key error and,
-            # when handling it, re-attempt inserts of the same rows one by one,
-            # discarding any that raise the duplicate key error.
-            for j, batch in enumerate(ids):
-                msg = 'Running {0} batch {1}, cumulative edges {2}'
-                msg = msg.format(type(self), j + 1, n_items)
-                logger.debug(msg)
-
-                rows = [
-                    {self.api_data_column: t, self.target_user_column: user.user_id}
-                    for t in batch
-                ]
-
-                try:
-                    self.session.bulk_insert_mappings(md.StgFollow, rows)
-                except sa.exc.IntegrityError:
-                    n_items += self.insert_one_by_one(md.StgFollow, rows)
-                else:
-                    n_items += len(batch)
-
-                # this slows things down, but without it, we'd lose every
-                # row loaded before one of these duplicate key problems
-                self.session.commit()
-
-            # First, load the users
-            flt = self.session.query(md.User).filter(
-                md.User.user_id == getattr(md.StgFollow, self.api_data_column)
-            ).correlate(md.StgFollow)
-
-            # We don't need to worry about inserting the same user_id value
-            # that's already in the user.user_id object (and causing a primary
-            # key integrity error on the user table) because that user_id is
-            # already in the self.target_user_column column; it would only also
-            # appear in the self.api_data_column column if you could follow
-            # yourself on Twitter, which you can't.
-            ins = md.User.__table__.insert().from_select(
-                ['user_id'],
-                self.session.query(
-                    getattr(md.StgFollow, self.api_data_column)
-                ).filter(~flt.exists())
-            )
-
-            self.session.execute(ins)
-
-            # Next, insert rows in StgFollow lacking a row in Follow with
-            # valid_end_dt of null
-            flt = self.session.query(md.Follow).filter(sa.and_(
-                md.Follow.valid_end_dt == None,
-                md.Follow.source_user_id == md.StgFollow.source_user_id,
-                md.Follow.target_user_id == md.StgFollow.target_user_id
-            )).correlate(md.StgFollow)
-
-            ins = md.Follow.__table__.insert().from_select(
-                ['source_user_id', 'target_user_id'],
-                self.session.query(
-                    md.StgFollow.source_user_id,
-                    md.StgFollow.target_user_id
-                ).filter(~flt.exists())
-            )
-
-            self.session.execute(ins)
-
-            # Lastly, update current Follow rows not found in StgFollow to set
-            # their valid_end_dt column to now()
-            flt = self.session.query(md.StgFollow).filter(sa.and_(
-                md.StgFollow.source_user_id == md.Follow.source_user_id,
-                md.StgFollow.target_user_id == md.Follow.target_user_id
-            )).correlate(md.Follow)
-
-            upd = md.Follow.__table__.update().where(sa.and_(
-                md.Follow.valid_end_dt == None,
-                getattr(md.Follow, self.target_user_column) == user.user_id,
-
-                ~flt.exists()
-            )).values(valid_end_dt=sa.func.now())
-
-            self.session.execute(upd)
+            n_items += self.load_edges_for(user)
 
             self.session.commit()
 
