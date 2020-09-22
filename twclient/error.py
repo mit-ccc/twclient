4d7a447c2a3bcebdf7e02b2ebbeeb37c6566d72e
William Brannon <will.brannon@gmail.com>
2020-09-22 00:57:43 -0400

    docstrings for error

--- twclient/error.py
+++ twclient/error.py
@@ -1,25 +1,37 @@
+'''
+Exceptions that twclient code may raise
+'''
+
 import logging
+
+import tweepy
 
 logger = logging.getLogger(__name__)
 
-##
-## Various types of errors we encounter
-##
-
-# The tweepy_is_instance methods are mostly right but may not cover all edge
-# cases, because Twitter's API documentation is frustratingly incomplete.
-
-# Note in particular that attempts to access protected users' friends,
-# followers, or tweets come back as an HTTP 401 with message "Not authorized."
-# and no Twitter status code.
-
-# See Twitter docs: https://developer.twitter.com/en/support/twitter-api/error-troubleshooting
-
-##
-## Base class for exceptions
-##
 
 class TWClientError(Exception):
+    '''
+    The base class for all errors raised by twclient.
+
+    Parameters
+    ----------
+    message : str
+        The reason for this error.
+
+    exit_status : int
+        If the exception is caught at the top-level command-line script,
+        this value is passed to `sys.exit`.
+
+    Attributes
+    ----------
+    message : str
+        The reason for this error.
+
+    exit_status
+        If the exception is caught at the top-level command-line script,
+        this value is passed to `sys.exit`.
+    '''
+
     def __init__(self, **kwargs):
         message = kwargs.pop('message', '')
         exit_status = kwargs.pop('exit_status', 1)
@@ -41,11 +53,36 @@ class TWClientError(Exception):
 
         return cls + '(' + arg_string + ')'
 
-##
-## Exceptions and functions related to the Twitter API
-##
 
+# See Twitter docs:
+# https://developer.twitter.com/en/support/twitter-api/error-troubleshooting
 class TwitterAPIError(TWClientError):
+    '''
+    Base class for errors returned by the Twitter API.
+
+    Instances of this class correspond to errors returned by the Twitter API,
+    but are higher-level and easier to handle in client code than the
+    underlying instances of tweepy.error.TweepError which gave rise to them.
+
+    Parameters
+    ----------
+    response : requests.Response object
+        The Twitter API response which led to this error being raised.
+
+    api_code : int
+        The API code Twitter returned.
+
+    Attributes
+    ----------
+    http_code
+
+    response : requests.Response object
+        The Twitter API response which led to this error being raised.
+
+    api_code : int
+        The API code Twitter returned.
+    '''
+
     _repr_attrs = ['message', 'exit_status', 'response', 'api_code',
                    'http_code']
 
@@ -60,56 +97,162 @@ class TwitterAPIError(TWClientError):
 
     @property
     def http_code(self):
+        '''
+        The HTTP status code Twitter returned with this error.
+        '''
+
         if self.response is not None:
             return self.response.status_code
-        else:
-            return None
+
+        return None
 
     @classmethod
     def from_tweepy(cls, ex):
-        return cls(message=ex.reason, response=ex.response, api_code=ex.api_code)
+        '''
+        Construct an instance from a tweepy exception object.
+
+        Parameters
+        ----------
+        ex : instance of tweepy.error.TweepError
+            The exception from which to generate a TwitterAPIError instance.
+
+        Returns
+        -------
+        Instance of the appropriate subclass of TwitterAPIError.
+        '''
+
+        return cls(message=ex.reason, response=ex.response,
+                   api_code=ex.api_code)
 
     @staticmethod
     def tweepy_is_instance(ex):
+        '''
+        Check whether a tweepy exception object can be converted to this class.
+
+        This method is intended to be implemented by subclasses as their core
+        piece of logic, so the implementation here raises NotImplementedError.
+
+        Parameters
+        ----------
+        ex : instance of tweepy.error.TweepError
+            The tweepy exception object to check.
+
+        Returns
+        -------
+        Instance of the appropriate subclass of TwitterAPIError.
+        '''
+
         raise NotImplementedError()
 
+
 class NotFoundError(TwitterAPIError):
+    '''
+    A requested object was not found.
+
+    There are several ways Twitter indicates that a requested object was not
+    found, involving some combination of the API response code, the HTTP status
+    code, and the message. Code in twclient generally can tell from context
+    what object was not found, so we combine these errors into one class.
+    '''
+
     @staticmethod
     def tweepy_is_instance(ex):
         return ex.api_code in (17, 34, 50, 63) or \
             (ex.api_code is None and ex.response.status_code == 404)
 
+
+# That is, accessing protected users' friends, followers, or tweets returns
+# an HTTP 401 with message "Not authorized." and no Twitter status code.
 class ProtectedUserError(TwitterAPIError):
+    '''
+    A requested user has protected tweets.
+
+    Requesting information about a user with protected tweets is not always an
+    error; certain kinds of information will be returned. But tweets and
+    friends/followers will not be and instead will raise this error.
+    '''
+
     @staticmethod
     def tweepy_is_instance(ex):
         return ex.api_code is None and ex.response.status_code == 401
 
+
 class CapacityError(TwitterAPIError):
+    '''
+    The Twitter API is temporarily over capacity.
+
+    This error is raised when Twitter's API indicates that it's over capacity
+    and cannot fulfill a request. Code catching this exception should generally
+    sleep a reasonable period of time and retry the request.
+    '''
+
     @staticmethod
     def tweepy_is_instance(ex):
         return ex.api_code in (130, 131) or \
             ex.response.status_code in (500, 503, 504)
 
-def dispatch(ex):
-    if isinstance(ex, TWClientError):
-        return ex
-    elif NotFoundError.tweepy_is_instance(ex):
-        return NotFoundError.from_tweepy(ex)
-    elif ProtectedUserError.tweepy_is_instance(ex):
-        return ProtectedUserError.from_tweepy(ex)
-    elif CapacityError.tweepy_is_instance(ex):
-        return CapacityError.from_tweepy(ex)
-    else:
+
+def dispatch_tweepy(ex):
+    '''
+    Take an exception class and convert it to a TWClientError if applicable.
+
+    This class takes in an arbitrary exception ex and dispatches it in the
+    following way: a) if ex is a tweepy.error.TweepError, convert it to the
+    corresponding TWClientError if possible, else b) return ex as-is. It is
+    used in wrappers of the Twitter API to simplify exception handling.
+
+    Parameters
+    ----------
+    ex : Exception
+        The exception instance to dispatch.
+
+    Returns
+    -------
+    Exception
+        The dispatched (possibly new) exception instance.
+    '''
+
+    if not isinstance(ex, tweepy.error.TweepError):
         return ex
 
-##
-## Other error conditions
-##
+    if NotFoundError.tweepy_is_instance(ex):
+        return NotFoundError.from_tweepy(ex)
+
+    if ProtectedUserError.tweepy_is_instance(ex):
+        return ProtectedUserError.from_tweepy(ex)
+
+    if CapacityError.tweepy_is_instance(ex):
+        return CapacityError.from_tweepy(ex)
+
+    return ex
+
 
 class SemanticError(TWClientError):
+    '''
+    Base class for non-Twitter error conditions.
+
+    These errors indicate larger problems with the operation of the program
+    than a specific Twitter or database error (though such an error may have
+    led to this one being raised).
+    '''
+
     pass
 
+
 class BadTargetError(SemanticError):
+    '''
+    A specified target user is protected, suspended or otherwise nonexistent.
+
+    This error is raised when a user targeted for `fetch` is found to be
+    unavailable. There may be several reasons for unavailability: a user having
+    protected tweets, being suspended, or otherwise not existing.
+
+    Parameters
+    ----------
+    targets : list of str or int
+        The Twitter user IDs or screen names causing the error.
+    '''
+
     def __init__(self, **kwargs):
         targets = kwargs.pop('targets', [])
 
@@ -117,14 +260,41 @@ class BadTargetError(SemanticError):
 
         self.targets = targets
 
+
 class BadTagError(SemanticError):
+    '''
+    A requested tag does not exist.
+
+    This error is raised when job.ApplyTagJob is given a tag which does not
+    exist in the database.
+
+    Parameters
+    ----------
+    tag : str
+        The name of the nonexistent tag.
+
+    Attributes
+    ----------
+    tag : str
+        The name of the nonexistent tag.
+    '''
+
     def __init__(self, **kwargs):
         tag = kwargs.pop('tag', None)
 
-        super(BadTargetError, self).__init__(**kwargs)
+        super(BadTagError, self).__init__(**kwargs)
 
         self.tag = tag
 
+
 class BadSchemaError(SemanticError):
+    '''
+    The database schema is corrupt or the wrong version.
+
+    This error is raised when a Job detects that the schema present in the
+    selected database profile is corrupt, an unsupported version, or not a
+    twclient schema.
+    '''
+
     pass
 
