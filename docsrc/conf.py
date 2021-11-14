c5ef20bac0ba4c8abe6ad9f7667cb866a5b94e2a
William Brannon <will.brannon@gmail.com>
2021-11-14 13:39:00 -0500

    remove unnecessary dirs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -49,9 +49,6 @@ extensions = [
 
 napoleon_google_docstrings = False
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 source_suffix = ['.rst', '.md']
@@ -105,11 +102,6 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 # html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
