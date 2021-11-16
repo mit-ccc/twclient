f61c4cd3bd8c1baa8058c265c0e1d141ae147900
William Brannon <will.brannon@gmail.com>
2021-11-15 19:43:58 -0500

    example of tests

--- test/twclient/test_utils.py
+++ test/twclient/test_utils.py
@@ -1,6 +1,18 @@
 '''
-Tests for utils.py
+Tests for utils.py.
 '''
 
-def test_foo():
-    assert 3 == 3
+import pytest
+from twclient import _utils as ut
+
+
+def test_split_camel_case():
+    with pytest.raises(TypeError):
+        ut.split_camel_case(None)
+
+    assert ut.split_camel_case('') == []
+    assert ut.split_camel_case('foobarbazquux') == ['foobarbazquux']
+    assert ut.split_camel_case('Foobarbazquux') == ['Foobarbazquux']
+    assert ut.split_camel_case('FooBarbazquux') == ['Foo', 'Barbazquux']
+    assert ut.split_camel_case('FooBarBazQuux') == ['Foo', 'Bar', 'Baz',
+                                                    'Quux']
