aa072635f725a947c4cddfd0fcec3b3b5bcd39ec
William Brannon <wbrannon@media.mit.edu>
2023-01-25 11:28:40 -0500

    docstrings for tests

--- test/twclient/test_utils.py
+++ test/twclient/test_utils.py
@@ -5,6 +5,9 @@ Tests for utils.py
 from src.twclient._utils import uniq
 
 def test_uniq():
-    assert uniq([1,1,2,2,3,3,4,4]) == [1, 2, 3, 4]
+    '''
+    Test twclient.utils.uniq
+    '''
 
+    assert uniq([1,1,2,2,3,3,4,4]) == [1, 2, 3, 4]
     assert uniq([1,2,1,5,9,7,4,4]) == [1,2,5,9,7,4]
--- test/twclient/utils.py
+++ test/twclient/utils.py
@@ -1,4 +1,12 @@
-def chunk_read(file, chunk_size=4096, mode='rb'):
+'''
+Utilities for twclient tests
+'''
+
+def chunk_read(file, chunk_size=4096):
+    '''
+    Yield successive chunks of a file-like object
+    '''
+
     while True:
         dat = file.read(chunk_size)
 
